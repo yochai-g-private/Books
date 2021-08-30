2dcb5c6aa9b0b306870ae12dc9cf943fe317113c
yochai-g-private <yochai.glauber@attunity.com>
2021-08-30 22:02:38 +0300

    OK

--- FreeNove/Sketches/Sketch_23.1.1_Infrared_Motion_Sensor/Sketch_23.1.1_Infrared_Motion_Sensor.ino
+++ FreeNove/Sketches/Sketch_23.1.1_Infrared_Motion_Sensor/Sketch_23.1.1_Infrared_Motion_Sensor.ino
@@ -6,13 +6,15 @@
   by http://www.freenove.com
 */
 
-int sensorPin = 11; // the number of the infrared motion sensor pin
-int ledPin = 12;    // the number of the LED pin
+int sensorPin = A4; // the number of the infrared motion sensor pin
+int ledPin = A1;    // the number of the LED pin
 
 void setup() {
-  Serial.begin(9600);
   pinMode(sensorPin, INPUT_PULLUP);  // initialize the sensor pin as input
   pinMode(ledPin, OUTPUT);    // initialize the LED pin as output
+  pinMode(A9,OUTPUT);
+  digitalWrite(A9, HIGH);
+  Serial.begin(115200);    // Open serial monitor at 9600 baud to see ping results.
 }
 
 int status = -1;
--- FreeNove/Sketches/Sketch_24.1.1_Ultrasonic_Ranging/Sketch_24.1.1_Ultrasonic_Ranging.ino
+++ FreeNove/Sketches/Sketch_24.1.1_Ultrasonic_Ranging/Sketch_24.1.1_Ultrasonic_Ranging.ino
@@ -6,9 +6,9 @@
   by http://www.freenove.com
 */
 
-#define trigPin A2 //12 // define TrigPin
-#define echoPin A3 //11 // define EchoPin.
-#define MAX_DISTANCE 200 // Maximum sensor distance is rated at 400-500cm.
+#define trigPin A5 //12 // define TrigPin
+#define echoPin A6 //11 // define EchoPin.
+#define MAX_DISTANCE 400 // Maximum sensor distance is rated at 400-500cm.
 // define the timeOut according to the maximum range. timeOut= 2*MAX_DISTANCE /100 /340 *1000000 = MAX_DISTANCE*58.8
 float timeOut = MAX_DISTANCE * 60; 
 int soundVelocity = 340; // define sound speed=340m/s
@@ -16,14 +16,39 @@ int soundVelocity = 340; // define sound speed=340m/s
 void setup() {
   pinMode(trigPin,OUTPUT);// set trigPin to output mode
   pinMode(echoPin,INPUT); // set echoPin to input mode
-  Serial.begin(9600);    // Open serial monitor at 9600 baud to see ping results.
+  pinMode(A9,OUTPUT);
+  digitalWrite(A9, HIGH);
+  Serial.begin(115200);    // Open serial monitor at 9600 baud to see ping results.
+  Serial.println("Started!");
 }
 
 void loop() {
-  delay(100); // Wait 100ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  delay(500); // Wait 100ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+#if 1
+  enum { MIND=130, MAXD=160, N_TIMES=4 };
+  static int in_range = 0;
+
+  int dist = getSonar();
+
+  Serial.println(dist);
+  if(dist >= MIND && dist <= MAXD)
+  {
+    in_range++;
+    
+    if(in_range < N_TIMES)
+      return;
+
+    Serial.println("*** BELL BELL BELL ***");
+  }
+  else
+  {
+    in_range = 0;
+  }
+#else
   Serial.print("Ping: ");
   Serial.print(getSonar()); // Send ping, get distance in cm and print result (0 = outside set distance range)
   Serial.println("cm");
+#endif
 }
 
 float getSonar() {
